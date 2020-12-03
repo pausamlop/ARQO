6189bf994ce468d17ec3bccf7081f6a1a36c1937
pabloizaguirre <pablo@izaguirre.eu>
2020-12-03 16:23:02 +0100

    Añadido -O3 al makefile

--- P3/ejercicio3.sh
+++ P3/ejercicio3.sh
@@ -7,7 +7,7 @@
 Ninicio=$((256+256*9))  # tiene que ser 256+256*9
 Nfinal=$((256+256*10))  # tiene que ser 256+256*10
 Npaso=32                
-Niter=20                 # tiene que ser 20
+Niter=10                 # tiene que ser 20
 fDAT=mult.dat
 fPNGC=mult_cache.png
 fPNGT=mult_time.png
